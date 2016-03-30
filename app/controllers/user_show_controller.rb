d135bb35cbbdcd99cc6d14b38a19fd4c80e12e4d
alran <Alyssa@Alyssas-MacBook-Air.local>
2016-03-30 14:43:16 -0400

    Add routes for edit, delete, and show user profile

--- app/controllers/user_show_controller.rb
+++ app/controllers/user_show_controller.rb
@@ -1,3 +1,19 @@
-get "/users/show" do
+# view profile
+get "/users/:id" do
+  @user = User.find_by(id: params[:id])
+  erb :'users/show'
+end
 
+# delete profile (not currently implemented)
+delete "/users/:id" do
+  #server request to delete user
+  #clear sessions
+  #back to homepage
+  redirect '/'
+end
+
+# edit profile (not currently implemented)
+get "/users/:id/edit" do
+  @user = User.find_by(id: params[:id])
+  erb :'users/edit'
 end
