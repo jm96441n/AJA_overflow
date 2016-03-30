50f5de314c0c9967c5db778b0efbd31f5a117eca
alran <Alyssa@Alyssas-MacBook-Air.local>
2016-03-30 16:23:26 -0400

    Add questions have many answers in database

---
+++ app/controllers/question_show_controller.rb
@@ -0,0 +1,20 @@
+#question show page route
+get '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  @asker = User.find_by(id: @question.user_id)
+  erb :'questions/show'
+end
+
+#delete question route
+delete 'questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  @question.destroy
+  redirect '/'
+end
+
+#edit question route
+get 'questions/:id/edit' do
+  @question = Question.find_by(id: params[:id])
+  @asker = User.find_by(@question.user_id)
+  erb :'questions/edit'
+end
