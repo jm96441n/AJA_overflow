d1f4a409d75a8dcf45f763a726e8ce3526a92dcd
John Maguire <jmaguire5588@gmail.com>
2016-03-30 18:37:02 -0400

    Update layout to link correctly, and display username when logged in, also finish log in and register

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
 get '/login' do
-
 	erb :'/sessions/new'
 end
 
 post '/login' do
-	user  = User.find_by(params)
+
+	user  = User.find_by(username: params[:user][:username])
+	if user && user.authenticate(params[:user][:password])
+		session[:user_id] = user.id
+		redirect '/'
+	else
+		@errors = ['Wrong username or password, try again']
+		erb :'sessions/new'
+	end
 
 end
+
+get '/logout' do
+	session.clear
+	redirect '/'
+end
+
