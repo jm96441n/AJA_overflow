f72817c354932e5aa19147d35c46290dda7c6260
akeem-s <akeemstreet@gmail.com>
2016-03-31 00:06:02 -0400

    ajax login post route working. we will want to change the jquery and styling when we finalize our layout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,17 @@ get '/login' do
 end
 
 post '/login' do
-
 	user  = User.find_by(username: params[:user][:username])
 	if user && user.authenticate(params[:user][:password])
-		session[:user_id] = user.id
-		redirect '/'
+		if request.xhr?
+			session[:user_id] = user.id
+			content_type :json
+      {username: params[:user][:username], password: params[:user][:password]}.to_json
+		else
+			session[:user_id] = user.id
+			redirect '/'
+		end
+
 	else
 		@errors = ['Wrong username or password, try again']
 		erb :'sessions/new'
@@ -24,3 +30,17 @@ get '/logout' do
 	redirect '/'
 end
 
+##to do 
+
+# #new question form still shows when you arent logged in
+# if request.xhr?
+#     @task.assign_attributes(name: params[:name], note: params[:note], list_id: params[:id])
+    
+#     if @task.save
+#       # redirect "/users/#{session[:logged_in_id]}"
+#       content_type :json
+#       {name: params[:name], note: params[:note]}.to_json
+#     else
+#       @errors = @task.errors.full_messages
+#       erb :'/tasks/_edit'
+#     end 
--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,63 @@
 $(document).ready(function(){
 
-  // up vote button on question page
-$("#up-vote-button").on("click",function(event){
-  event.preventDefault();
+    // up vote button on question page
+  $("#up-vote-button").on("click",function(event){
+    event.preventDefault();
 
-  var $target = $(event.target);
+    var $target = $(event.target);
 
-  $.ajax({
-    url: $target.attr('href'),
-    method: 'get',
-    data: {action: "up-vote"}
-  }).done(function(response){
-    var votes_count = response.votes;
-    $("#vote-count").text(votes_count);
-  }).fail(function(){
-    //raise error
+    $.ajax({
+      url: $target.attr('href'),
+      method: 'get',
+      data: {action: "up-vote"}
+    }).done(function(response){
+      var votes_count = response.votes;
+      $("#vote-count").text(votes_count);
+    }).fail(function(){
+      //raise error
+    });
   });
+
+    // render login form on index/home page
+  $("#login-link").on("click", function(event){
+    event.preventDefault();
+
+    var path = $("a[href='/login']").attr("href");
+
+    $.ajax({
+      url: path,    
+      method: "get"
+    }).done(function(response){
+      $("#user-login-form-placeholder").html(response);
+      $("#new_question_form").fadeOut("10000"); 
+      $("#questions-list-section").fadeOut("10000");
+    });
+   });
+
+  $("#user-login-form-placeholder").on("submit", "#login-form", function(event){
+    event.preventDefault();
+    var path = $(event.target).attr("action");
+    var formData = $(event.target).serialize();
+
+    $.ajax({
+      url: path, 
+      type: "post",
+      data: formData,
+      dataType: "html"
+    })
+    .done(function(response){
+      var parsedResponse = JSON.parse(response);
+      // $("#user-login-form-placeholder").hide();
+      // $("#new_question_form").show();
+      // $("#questions-list-section").show();
+      location.reload();
+    })
+  })
+
 });
 
-  // render login form on index/home page
-$("#login-link").on("click", function(event){
-  event.preventDefault();
 
-  var path = $("a[href='/login']").attr("href");
+// var parsedResponse = JSON.parse(response);
+//       $("#tasks_details_aside h1").html(parsedResponse.name);
+//       $("#tasks_details_aside h3").html(parsedResponse.note);
 
-  $.ajax({
-    url: path,    
-    method: "get"
-  }).done(function(response){
-    $("#user-login-form-placeholder").append(response);
-    $("body").css("background-color", "black")
-    $("#new_question_form").hide(); // just fucking around w/ this and line 32 until fix issue detailed below.
-    // $("#user-login-form-placeholder-background").show(); // I added this div to change the background color of a div covering the whole screen with opacity. it might not be working because of foundation's css. will figure out tomorrow.
-  });
- });
-});
