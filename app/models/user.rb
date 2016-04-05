e75532ac67daff94509f5cfa34564e9be2b16aad
akeem-s <akeemstreet@gmail.com>
2016-04-04 22:59:08 -0400

    fix user model methods for favorite questions and answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,55 +10,15 @@ class User < ActiveRecord::Base
   validates :recovery_first_pet, :recovery_first_street, :recovery_mom_maiden_name, :last_name, :first_name, :password_digest, presence: true
   validates :username, :email, uniqueness: true, presence: true
 
-
   def find_by(user_id)
     User.find_by(id: user_id)
   end
 
-  def favorited_answers 
-    favorites = Favorite.all
-    answers = Answer.all
-    answer_array = []
-    fav_array = []
-
-    favorites.each do |fav|
-      if self.id == fav.user_id && fav.favoritable_type == "Answer"
-        fav_array << fav
-      end
-    end
-
-    fav_array.each do |fav|
-      answers.each do |ans|
-        if fav.favoritable_id == ans.id
-          answer_array << ans
-        end
-      end
-      answer_array
-    end
-    answer_array
+  def fav_ans
+    favorites.where(favoritable_type: "Answer").join('answers')
   end
 
-  def favorited_questions 
-    favorites = Favorite.all
-    question = Question.all
-    question_array = []
-    fave_array = []
-
-    favorites.each do |fav|
-      if self.id == fav.user_id && fav.favoritable_type == "Question"
-        fave_array << fav
-      end
-    end
-
-    fave_array.each do |fav|
-      questions.each do |ques|
-        if fav.favoritable_id == ques.id
-          question_array << ques
-        end
-      end
-      question_array
-    end
-    question_array
+  def fav_ques
+    favorites.where(favoritable_type: "Answer").join('questions')
   end
-
 end
